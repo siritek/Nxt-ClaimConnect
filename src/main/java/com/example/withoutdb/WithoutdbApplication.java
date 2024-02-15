fef9285e11043683f423a63d66ac0c700fc581a9
shanthanakkiraju <shanthanakkiraju@gmail.com>
2024-02-15 15:29:13 -0500

    new changes

--- src/main/java/com/example/withoutdb/WithoutdbApplication.java
+++ src/main/java/com/example/withoutdb/WithoutdbApplication.java
@@ -2,13 +2,18 @@ package com.example.withoutdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WithoutdbApplication {
+public class WithoutdbApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WithoutdbApplication.class, args);
 	}
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WithoutdbApplication.class);
+	}
 }
