8b88c5e152f13a68683229e8446a440244fbf0a5
shanthanakkiraju <shanthanakkiraju@gmail.com>
2024-02-22 00:47:53 -0500

    Aws RDS config made in DBConn and App.properties

--- src/main/java/com/example/withoutdb/service/DBConn.java
+++ src/main/java/com/example/withoutdb/service/DBConn.java
@@ -11,8 +11,8 @@ public class DBConn {
         Connection conn = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //conn = DriverManager.getConnection("jdbc:mysql://nxt-database.cp8kqw48c26v.us-east-1.rds.amazonaws.com:3306/nxtdb?useSSL=false&allowPublicKeyRetrieval=true&autoReconnect=true&failOverReadOnly=false&maxReconnects=1500", "root", "root");
-            conn = DriverManager.getConnection("jdbc:mysql://nxt-database.cp8kqw48c26v.us-east-1.rds.amazonaws.com:3306/nxtdb", "admin", "password123");
+            //conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/nxtbase_db?useSSL=false&allowPublicKeyRetrieval=true&autoReconnect=true&failOverReadOnly=false&maxReconnects=1500", "root", "root");
+            conn = DriverManager.getConnection("jdbc:mysql://nxt-database.cp8kqw48c26v.us-east-1.rds.amazonaws.com:3306/nxtbase_db", "admin", "password123");
 
         }
         catch(Exception ex)
